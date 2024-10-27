3b0bbc0248e0c059632fb7d9481631c40ac5ee6f
vivek vishal <vivek1557.be21@chitkara.edu.in>
2024-10-27 20:19:38 +0530

    implemet hero section

---
+++ components/AnimatedCounter.tsx
@@ -0,0 +1,16 @@
+﻿'use client'
+import React from 'react'
+import CountUp from 'react-countup'
+
+function AnimatedCounter({ amount }: { amount: number }) {
+    return (
+        <CountUp className='w-full' end={amount}
+            prefix='$'
+            decimal=','
+            duration={2}
+            decimals={2}
+        />
+    )
+}
+
+export default AnimatedCounter
---
+++ components/DoughNutChart.tsx
@@ -0,0 +1,29 @@
+﻿'use client'
+import { Chart as ChartJS, ArcElement, Legend, Tooltip } from 'chart.js';
+import React from 'react'
+import { Doughnut } from 'react-chartjs-2';
+ChartJS.register(ArcElement, Tooltip, Legend)
+function DoughNutChart({ accounts }: DoughnutChartProps) {
+    const data = {
+        datasets: [
+            {
+                label: "Banks",
+                data: [1250, 4900, 3750],
+                backgroundColor: ['#0747b6', '#2265d8', '#2f91fa']
+            }
+        ],
+        label: ['Bank 1', 'Bank 2', 'Bank 3']
+    }
+    return <Doughnut data={data}
+        options={{
+            cutout: "70%",
+            plugins: {
+                legend: {
+                    display: false
+                }
+            }
+        }}
+    />
+}
+
+export default DoughNutChart
